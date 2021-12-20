5f69938bef1d2a17bdd0bae0457f3ec4a0abf2f8
gm1237 <96093679+gm1237@users.noreply.github.com>
2021-12-20 10:40:49 +0900

    123

---
+++ CGV/script.js
@@ -0,0 +1,26 @@
+const suiteCinema = document.querySelector(".suite-cinema");
+const skyBox = document.querySelector(".sky-box");
+const dX = document.querySelector(".DX");
+const cineDechef = document.querySelector(".cine-de-chef");
+
+const suitePicture = document.querySelector(".picture1");
+const skyPicture = document.querySelector(".picture2");
+const dxPicture = document.querySelector(".picture3");
+const cinePicture = document.querySelector(".picture4");
+
+/* 마우스 hover시에 좌측 사진변화 */
+suiteCinema.addEventListener("mouseover", () => {
+  console.log("click");
+});
+
+suiteCinema.addEventListener("mouseover", () => {
+  console.log("click");
+});
+
+suiteCinema.addEventListener("mouseover", () => {
+  console.log("click");
+});
+
+suiteCinema.addEventListener("mouseover", () => {
+  console.log("click");
+});
