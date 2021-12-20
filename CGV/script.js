66069d183a29c41be4682563a1d671964a49a330
gm1237 <96093679+gm1237@users.noreply.github.com>
2021-12-20 11:13:40 +0900

    90% done

--- CGV/script.js
+++ CGV/script.js
@@ -3,24 +3,25 @@ const skyBox = document.querySelector(".sky-box");
 const dX = document.querySelector(".DX");
 const cineDechef = document.querySelector(".cine-de-chef");
 
-const suitePicture = document.querySelector(".picture1");
-const skyPicture = document.querySelector(".picture2");
-const dxPicture = document.querySelector(".picture3");
-const cinePicture = document.querySelector(".picture4");
+const suitePicture = document.querySelector(".suite-picture");
 
 /* 마우스 hover시에 좌측 사진변화 */
 suiteCinema.addEventListener("mouseover", () => {
-  console.log("click");
+  suitePicture.innerHTML =
+    '<div class="picture"><img src="./image1/suite cinema.png" alt=""></div>';
 });
 
-suiteCinema.addEventListener("mouseover", () => {
-  console.log("click");
+skyBox.addEventListener("mouseover", () => {
+  suitePicture.innerHTML =
+    '<div class="picture"><img src="./image1/sky box.png" alt=""></div>';
 });
 
-suiteCinema.addEventListener("mouseover", () => {
-  console.log("click");
+dX.addEventListener("mouseover", () => {
+  suitePicture.innerHTML =
+    '<div class="picture"><img src="./image1/4DX.png" alt=""></div>';
 });
 
-suiteCinema.addEventListener("mouseover", () => {
-  console.log("click");
+cineDechef.addEventListener("mouseover", () => {
+  suitePicture.innerHTML =
+    '<div class="picture"><img src="./image1/cine de chef.png" alt=""></div>';
 });
